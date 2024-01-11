67e1fbdeedb737edd304ab7319fbe47a0171c05c
Neematrasheed05 <nymatrasheed05@gmail.com>
2024-01-11 06:59:14 +0300

    complete assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,24 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "http://makeup-api.herokuapp.com/api/v1/products.json"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    product_list = []
+    products = json.loads(self.get_programs())
+    for product in products:
+            product_list.append(product["product_type"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+    return product_list
+
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for product in set(programs_schools):
+    print(product)
